4ed214205b950d3b22ab00f1d1a435511fb08435
Rituka Jadhav <ritukajadhav678@gmail.com>
2025-12-05 17:32:10 +0000

    commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 9089 //declaring constants 
+
+//declaring variables
+
+let accountName = "Rituka"
+let email = "abc@gmail.com"
+let state = "Maharashtra"
+let city = "Pune"
+
+// print statemet 1
+console.log(accountID)
+
+
+//tabular form print
+console.table([accountID,accountName,email,state,city])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+var accountPassword = "12345"
+*/
